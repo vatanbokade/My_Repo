5d1ff9fe09883d668df9d4bb4fe646520ce46b1d
vatangb <vatanbokade@gmail.com>
2021-10-07 22:26:10 +0530

    autogit

--- pipelines/pipeline.groovy
+++ pipelines/pipeline.groovy
@@ -1,1 +1,14 @@
-Demo File.
+node {  
+    stage('Pull') { 
+        git branch: 'main', url: 'https://github.com/vatanbokade/My_Repo.git'
+    }
+    stage('Build') { 
+        echo "Build Done"
+    }
+    stage('Test') { 
+        echo "Test Done"
+    }
+    stage('Deploy') { 
+        echo "Deploy Done"
+    }
+}
